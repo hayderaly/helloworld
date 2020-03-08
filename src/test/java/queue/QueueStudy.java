23a4cf322d7377a8f0f31450b8ffd0b07db8d2eb
haidongtest <645765423@qq.com>
2020-03-08 20:48:53 +0800

    hello ssh github

---
+++ src/test/java/queue/QueueStudy.java
@@ -0,0 +1,10 @@
+package queue;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class QueueStudy {
+
+    public static void main(String[] args){
+        new LinkedBlockingDeque<Object>();
+    }
+}
