5ca9fef6bfd991983c71dd6cd8e6ef090f666ad3
hayderwang <645765423@qq.com>
2020-03-08 14:09:53 +0800

    hello github

---
+++ src/main/java/com/demo/controller/HelloWorldMainApplication.java
@@ -0,0 +1,12 @@
+package com.demo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args){
+        //Spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
