40fc450e55fcd907ba50fa77fbc03e5233544475
haidongtest <645765423@qq.com>
2020-03-22 11:56:56 +0800

    change user name

--- src/main/java/com/demo/controller/HelloWorldMainApplication.java
+++ src/main/java/com/demo/controller/HelloWorldMainApplication.java
@@ -10,6 +10,7 @@ public class HelloWorldMainApplication {
         //rebase test
         //origin or local test
         //new user
+        //change user name
         SpringApplication.run(HelloWorldMainApplication.class,args);
     }
 }
